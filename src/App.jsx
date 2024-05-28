fa3d0793658c532a6393d90cbf3a7a44b755688d
absim29 <simon.bianca29@gmail.com>
2024-05-28 17:16:52 +0200

    Update app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Contact from './pages/ContactPage';
+import Cart from './pages/CheckoutPage';
+import Home from './pages/HomePage';
+import { Route, Routes } from 'react-router-dom';
+import Post from './pages/ProductPage';
+import Navbar from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Contact' element={<Contact />} />
+          <Route path='/Checkout' element={<Cart />} />
+          <Route path='/post/:id' element={<Post />} />
+          <Route path='*' element={<h1>Page not found</h1>} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
