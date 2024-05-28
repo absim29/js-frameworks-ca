7983a3d76e286874e8923732d61f22875aec597c
absim29 <simon.bianca29@gmail.com>
2024-05-28 19:09:38 +0200

    Add cart icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return <nav className="nav">
@@ -6,7 +8,7 @@ export default function Navbar() {
     <ul>
       <li><NavLink to="/" className={({ isActive }) => (isActive ? 'li active' : 'li')}>Home</NavLink> </li>
       <li><NavLink to="/Contact">Contact</NavLink></li>
-      <li><NavLink to="/Checkout">Cart</NavLink></li>
+      <li><NavLink to="/Checkout"><FontAwesomeIcon icon={faShoppingCart} /></NavLink></li>
     </ul>
   </nav>
 }
