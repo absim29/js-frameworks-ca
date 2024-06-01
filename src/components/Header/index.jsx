8525732556c654af38fcaafe3b4db6cdfc39d8b2
absim29 <simon.bianca29@gmail.com>
2024-06-01 22:56:36 +0200

    navbar adjustments

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,15 +8,15 @@ export default function Navbar() {
   return (
     <>
   <nav className="nav">
+    <div>
     <div className="logo">eCOMstore</div>
     <ul>
       <li><NavLink to="/" className={({ isActive }) => (isActive ? 'li active' : 'li')}>Home</NavLink> </li>
       <li><NavLink to="/Contact">Contact</NavLink></li>
       <li><NavLink to="/Checkout"><FontAwesomeIcon icon={faShoppingCart} /></NavLink></li>
     </ul>
-    
+    </div>
     <SearchBar />
-    
   </nav>
   </>
   );
