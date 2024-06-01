69a89c852ec8c9b3211227e2976d773a66cb85c4
absim29 <simon.bianca29@gmail.com>
2024-06-01 22:56:14 +0200

    star rating

---
+++ src/components/StarRating/index.jsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faStarHalfAlt, faStar as farStar } from "@fortawesome/free-solid-svg-icons";
+
+const StarRating = ({ rating }) => {
+  const fullStars = Math.floor(rating);
+  const halfStar = rating % 1 !== 0;
+  const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+
+  return (
+    <>
+      {[...Array(fullStars)].map((_, index) => (
+        <FontAwesomeIcon key={index} icon={faStar} />
+      ))}
+      {halfStar && <FontAwesomeIcon icon={faStarHalfAlt} />}
+      {[...Array(emptyStars)].map((_, index) => (
+        <FontAwesomeIcon key={index} icon={farStar} />
+      ))}
+    </>
+  );
+};
+
+export default StarRating;
