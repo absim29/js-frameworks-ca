ac57a6bbe8d029121b0eb092763b40de056f5d0f
absim29 <simon.bianca29@gmail.com>
2024-06-03 16:32:17 +0200

    Update index.jsx

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -3,14 +3,22 @@ import { Link } from "react-router-dom";
 import { useCart } from "../../context/Shop";
 
 export default function CartComponent() {
-  const { cart, removeFromCart, clearCart } = useCart();
+  const { cart, removeFromCart, clearCart, incrementQuantity, decrementQuantity } = useCart();
 
   const handleRemove = (itemId) => {
     removeFromCart(itemId);
   };
 
-  const totalPrice = cart.reduce((total, item) => total + item.discountedPrice, 0);
+  const handleIncrement = (itemId) => {
+    incrementQuantity(itemId);
+  };
 
+  const handleDecrement = (itemId) => {
+    decrementQuantity(itemId);
+  };
+
+  const totalPrice = cart.reduce((total, item) => total + item.discountedPrice * item.quantity, 0);
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <>
@@ -29,13 +37,17 @@ export default function CartComponent() {
                 <li key={item.id}>
                   <img src={item.image.url} alt={item.image.alt || item.title} className="image" />
                   {item.title} - ${item.discountedPrice.toFixed(2)}
+                  <button onClick={() => handleDecrement(item.id)}>-</button>
+                  <input value={item.quantity} />
+                  <button onClick={() => handleIncrement(item.id)}>+</button>
                   <button onClick={() => handleRemove(item.id)}>Remove</button>
                 </li>
               ))}
             </ul>
+            <p>Total Items: {totalItems}</p>
             <p>Total: ${totalPrice.toFixed(2)}</p>
-            <Link to={"/Success"}>
-              <button className="checkout-btn" onClick={clearCart}>Checkout</button>
+            <Link to={"/Success"} onClick={clearCart}>
+              <button className="checkout-btn">Checkout</button>
             </Link>
           </>
         )}
