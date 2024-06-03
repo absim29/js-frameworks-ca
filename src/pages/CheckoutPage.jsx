b28ebfb6b2dd51d1d19baeb676bea2b97db00b63
absim29 <simon.bianca29@gmail.com>
2024-06-03 14:08:48 +0200

    Add Cart component

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,15 +1,11 @@
-import { Link } from "react-router-dom";
+import CartComponent from "../components/Cart";
 
 export default function Cart() {
   return (
   <>
   <h1>Cart - Checkout</h1>
   <div className='prodContainer'>
-    {/* <div className="btn-wrap"> */}
-      <Link to={"/Success"}>
-        <button className="checkout-btn">Checkout</button>
-      </Link>
-    {/* </div> */}
+      <CartComponent />
     </div>
   </>
 )}
