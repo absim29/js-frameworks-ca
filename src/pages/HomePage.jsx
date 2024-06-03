d566d1662806814949733f7ee7b401b2178d2689
absim29 <simon.bianca29@gmail.com>
2024-06-03 12:02:25 +0200

    use Product on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
+import Product from "../components/Product";
 
 const url = 'https://v2.api.noroff.dev/online-shop';
 
@@ -13,28 +13,13 @@ function Home() {
   if (isError) {
     return <div>Error loading data</div>;
   }
-  console.log(data)
+
   return (
     <>
     <h1>Welcome to our store</h1>
     <div className="mainContainer">
       {data.map((item) => (
-        <div key={item.id} className="box">
-          <div className="card">
-            <div>
-            <img src={item.image.url} alt={item.image.alt || item.title} className="image" />
-            <h2>{item.title}</h2>
-            </div>
-            <div>
-            <p>{item.description}</p>
-            <p>Price: ${item.price.toFixed(2)}</p>
-            <p>Discounted Price: ${item.discountedPrice.toFixed(2)}</p>
-          <Link to={`/post/${item.id}`}>
-            <button className="btn">View</button>
-          </Link>
-          </div>
-          </div>
-        </div>
+        <Product key={item.id} item={item} />
       ))}
     </div>
     </>
