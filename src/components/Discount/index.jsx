2f4a081684b3a6b5052ab4e490087624cc11fc6c
absim29 <simon.bianca29@gmail.com>
2024-06-03 12:01:52 +0200

    reusable discount function

---
+++ src/components/Discount/index.jsx
@@ -0,0 +1,8 @@
+
+const calculateDiscount = (price, discountedPrice) => {
+  return price > discountedPrice 
+      ? Math.ceil(((price - discountedPrice) / price) * 100) 
+      : 0;
+};
+
+export default calculateDiscount;
