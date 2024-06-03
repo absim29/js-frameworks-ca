b56c5aff4cd1e4396b72a390546bb0f97cc4c801
absim29 <simon.bianca29@gmail.com>
2024-06-03 16:32:06 +0200

    design adjustments

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -7,22 +7,23 @@ const Product = ({ item }) => {
   const { addToCart } = useCart();
 
   return (
+  <>
     <div className="box">
       <div className="card">
-        <div>
-          <img src={item.image.url} alt={item.image.alt || item.title} className="image" />
-          <h2>{item.title}</h2>
-        </div>
-        <div>
-          <p>{item.description}</p>
-          <p className="newprice">Price: ${item.discountedPrice.toFixed(2)}</p>
-          <Link to={`/post/${item.id}`}>
-            <button className="btn">View</button>
-          </Link>
-          <button className="btn" onClick={() => addToCart(item)}>Buy</button>
-        </div>
+        <Link to={`/post/${item.id}`}>
+          <div>
+            <img src={item.image.url} alt={item.image.alt || item.title} className="image" />
+            <h2>{item.title}</h2>
+          </div>
+          <div>
+            <p>{item.description}</p>
+            <p className="newprice">Price: ${item.discountedPrice.toFixed(2)}</p>
+          </div>
+        </Link>
+        <button className="btn" onClick={() => addToCart(item)}>Add to cart</button>
       </div>
     </div>
+  </>
   );
 };
 
