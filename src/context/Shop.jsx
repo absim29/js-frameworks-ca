1bc50d446f5c224235a5eca59afb569ebb12d23a
absim29 <simon.bianca29@gmail.com>
2024-06-03 16:31:08 +0200

    increment and decrement items in cart

--- src/context/Shop.jsx
+++ src/context/Shop.jsx
@@ -8,7 +8,14 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
+    const existingItemIndex = cart.findIndex((item) => item.id === product.id);
+    if (existingItemIndex !== -1) {
+      const updatedCart = [...cart];
+      updatedCart[existingItemIndex].quantity += 1;
+      setCart(updatedCart);
+    } else {
+      setCart((prevCart) => [...prevCart, { ...product, quantity: 1 }]);
+    }
   };
 
   const removeFromCart = (productId) => {
@@ -19,8 +26,26 @@ export const CartProvider = ({ children }) => {
     setCart([]);
   };
 
+  const incrementQuantity = (productId) => {
+    setCart((prevCart) => 
+      prevCart.map((item) => 
+        item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decrementQuantity = (productId) => {
+    setCart((prevCart) => 
+      prevCart.map((item) => 
+        item.id === productId && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, incrementQuantity, decrementQuantity }}>
       {children}
     </CartContext.Provider>
   );
