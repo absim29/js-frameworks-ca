1cd8d6b60a324e3cc153bf5441847490b3321e5a
absim29 <simon.bianca29@gmail.com>
2024-06-03 16:32:29 +0200

    Header cart count

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,13 @@ import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 import SearchBar from "../SearchBar";
+import { useCart } from "../../context/Shop";
 
 export default function Navbar() {
+
+  const { cart } = useCart();
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <>
   <nav className="nav">
@@ -13,7 +18,14 @@ export default function Navbar() {
     <ul>
       <li><NavLink to="/" className={({ isActive }) => (isActive ? 'li active' : 'li')}>Home</NavLink> </li>
       <li><NavLink to="/Contact">Contact</NavLink></li>
-      <li><NavLink to="/Checkout"><FontAwesomeIcon icon={faShoppingCart} /></NavLink></li>
+      <li>
+          <NavLink to="/Checkout">
+            <div className="cart-icon">
+              <FontAwesomeIcon icon={faShoppingCart} />
+                {totalItems > 0 && <div className="cart-overlay">{totalItems}</div>}
+            </div>
+          </NavLink>
+      </li>
     </ul>
     </div>
     <SearchBar />
